be2eb07e6824015ef04f6c65808f99b8ce486096
Manitej-04 <manitejb04@gmail.com>
2025-02-01 11:41:23 +0530

    Hello World Program

---
+++ PDemo/PDemo003.java
@@ -0,0 +1,17 @@
+public class PDemo003 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = 30;
+
+        if((x>y)&&(x>z)){
+            System.out.println("x is greater than y and z");
+        }
+        else if((y>x)&&(y>z)){
+            System.out.println("y is greater than x and z");
+        } else{
+            System.out.println("z is greater than x and y");
+        }
+
+    }
+}
